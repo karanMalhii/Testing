90924d378dd4f7503c1527b4803aaa5a78946383
karanMalhii <karanmalhii1313@gmail.com>
2024-04-12 17:57:37 +0530

    spinner don't fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import Navigation from "./Components/Navigation";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner } from "flowbite-react";
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+});
 
 export default function App() {
-  return <div>Testing
-    <Navigation/>
-  </div>;
+  const { data, isLoading } = useQuery({
+    queryKey: ["gameHub"],
+    queryFn: async function () {
+      const response = await axiosInstance.get(
+        "/games?key=0bbabdb992314fd3beab39914f8cb708&page=1"
+      );
+      console.log(response.data.results);
+      return response.data.results;
+    },
+  });
+  return (
+    <div>
+      <Navigation />
+      {isLoading && <Spinner size="lg" />}
+
+      {data?.map((item, index) => {
+        return <div key={index}>{item.name}</div>;
+      })}
+    </div>
+  );
 }
